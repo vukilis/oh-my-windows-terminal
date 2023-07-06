96e60041a7c0f78e73c6a5049a3483b6f3fe45be
vukilis <vuklekic22@gmail.com>
2023-07-06 23:20:17 +0200

    commit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" --print) -join "`n"))
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_vuk1lis.omp.json" --print) -join "`n"))
 
 Import-Module -Name Terminal-Icons
 Import-Module -Name PSReadLine
